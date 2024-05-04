bc31f0b7f7a82103b9d4e80740dd9844f021c7c8
Vivek7411 <vivnehra74@gmail.com>
2024-05-04 14:39:29 +0530

    linked list program

--- linkedlist.c
+++ linkedlist.c
@@ -88,7 +88,7 @@ void insert_after(node **head1, int item, int val)
 
 void show(node *head1)
 {
-    node *temp = head1;
+    node *temp = head1; // no need of temp.............?????
     while (temp != NULL)
     {
         printf("%d->", temp->info);
@@ -114,8 +114,214 @@ void main()
     show(head);
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //    node head1 = (node)malloc(sizeof(node));
 //     head->info = 10;
 //     head->next = head1;
 //     head1->info = 20;
 //     head1->next = NULL;
+
+/*
+
+
+                        #include<stdio.h>
+#include<Stdlib.h>
+typedef struct link{
+    int info;
+    struct link *next;
+}node;
+
+node* search(node *head, int item){
+   while(head != NULL)
+   {
+    if(head->info == item)
+    {
+        return head;
+    }else{
+        head = head->next;
+    }
+   }
+}
+void insert_at_beg(node **head1, int val)
+{
+    node temp = (node)malloc(sizeof(node));
+    temp->info = val;
+    if(*head1 == NULL)
+    {
+      temp->next = NULL;
+    }else{
+        temp->next = *head1;
+    }
+
+    *head1 = temp;
+}
+
+void insert_at_end(node **head1, int val){
+
+    node temp = (node)malloc(sizeof(int));
+    temp->info = val;
+    temp->next = NULL;
+    if(*head1 == NULL){
+
+      *head1 = temp;
+    }else{
+        node *loc = *head1;
+        while(loc->next != NULL){
+
+           loc = loc->next;
+        }
+        loc->next = temp;
+    }
+}
+
+
+void insert_after(node **head1,int item, int val){
+   node *loc = search(*head1, 3);
+    if (loc == NULL) {
+        printf("item not found");
+        return;
+    }
+   node temp = (node)malloc(sizeof(node));
+   temp->info = val;
+   temp->next = loc->next;
+   loc->next = temp;
+}
+
+void traverse(node *head1){
+    node *temp = head1;
+    if(temp == NULL){
+    printf("no modes found");
+    }
+    while(temp != NULL)
+    {
+        printf("%d->", temp->info);
+        temp = temp->next;
+    }
+}
+
+void delete_at_beg(node **head1){
+    node *temp = *head1;
+    *head1 = (*head1)->next;
+    free(temp);
+}
+
+void delete_at_end(node**head1){
+    node *temp = (*head1);
+    node *loc;
+    if(temp->next == NULL)
+    {
+        loc = (*head1);
+        (*head1) = NULL;
+        free(loc);
+
+    }else{
+        loc = *head1;
+    while(loc->next != NULL)
+    {
+      temp = loc;
+      loc = loc->next;
+    }
+     temp->next = NULL;
+     free(loc);
+    }
+}
+
+void delete_after(node **head1, int val)
+{
+    node *loc = search(*head1, val);
+    node *temp = loc->next;
+    loc->next = temp->next;
+    free(temp);
+
+}
+
+void delete_entire(node **head1){
+    node *temp = *head1;
+    node *loc;
+    while(temp != NULL){
+        loc = temp->next;
+        free(temp);
+        temp = loc;
+    }
+    *head1 = NULL;
+}
+
+
+
+void main()
+{
+//     int op;
+//     while(1){
+
+//     printf("1: Insert at begining\n");
+//     printf("2: Insert after element\n");
+//     printf("3: Insert at end\n");
+//     printf("4: traverse\n");
+//     printf("5: traverse in reverse\n");
+//     printf("6: Delete at begining\n");
+//     printf("7: Delete at end\n");
+//     printf("8: Delete after element\n");
+//     printf("9: delete entire list\n");
+//     printf("10: search an element\n");
+//     printf("11: Reverse the list\n");
+//     printf("12: Exit\n");
+//     printf("\nEnter your choice: \n");
+//     scanf("%d",op);
+
+//     break;
+//     }
+
+       node *head = NULL;
+
+insert_at_beg(&head, 3);
+insert_at_beg(&head, 'a');
+insert_at_beg(&head, 2);
+insert_at_beg(&head, 1);
+insert_at_end(&head, 4);
+// delete_at_beg(&head);
+insert_after(&head,3, 5);
+// delete_at_end(&head);
+delete_after(&head, 97);
+delete_entire(&head);
+
+traverse(head);
+printf("*");
+}
+
+
+
+
+
+
+
+//    node head1 = (node)malloc(sizeof(node));
+//     head->info = 10;
+//     head->next = head1;
+//     head1->info = 20;
+//     head1->next = NULL;
+
+
+*/
---
+++ sllMenu.c
@@ -0,0 +1,448 @@
+// Role of head1 is to point to the head pointer.
+// *head1 means value at(derefeer) address which is stored in head1;
+// means the value stored at head pointer.
+// when changing value use call by pointer else use call by val(in which head addres will be given.)
+// head and *head1 points to same thing i.e first node.
+// *head1 cant be used with -> so store *head1 in temp or use (*head1)-> why used in delete.
+// when trying to free memeonry call by pointer **node is recommende cuz issue in freeing memory then.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int item;
+    struct node *next;
+} node;
+
+void insertAtStart(node **, int);
+void insertAtEnd(node **, int);
+void show(node *);
+void insertAfter(node **, int, int);
+node *search(node *, int);
+void deleteAtStart(node **);
+void deleteAtEnd(node **);
+void insertBefore(node **, int, int);
+void deleteList(node **);
+
+// void deleteAtEnd2(node *);
+// void insertBefore(node **,int,int); // i belive insert before is not possiable in sll.
+
+void main()
+{
+
+    int c, val, item;
+    node *head = NULL; // must declare outside loop.
+
+    while (1)
+    {
+
+        printf("\n******MENU********\n");
+        printf("\n1.Traverse");          // 1
+        printf("\n2.Insert at start");   // 1
+        printf("\n3.Insert at end");     // 1
+        printf("\n4.Delete from start"); // 1
+        printf("\n5.Delete from end");   // 1
+        printf("\n6.Insert after");      // 1
+        printf("\n7.Insert Before");     // 1
+        printf("\n8.Delete entire list");
+        printf("\n9.Search"); // 1
+        printf("\n10.Reverse");
+        printf("\n11.Delete after");
+        printf("\n12.Delete before");
+        printf("\n13.Exit\n"); // 1
+        printf("\nChoose a option from the menu : ");
+        scanf("%d", &c);
+
+        switch (c)
+        {
+        case 1:
+            printf("\nYou chose to traverse the linked list\n");
+            show(head); // only head means addres of first node.
+            break;
+
+        case 2:
+            printf("\nEnter the value you wanted to add at start : ");
+            scanf("%d", &val);
+            insertAtStart(&head, val);
+            break;
+
+        case 3:
+            printf("\nEnter the value you wanted to add at end : ");
+            scanf("%d", &val);
+            insertAtEnd(&head, val);
+            break;
+
+        case 4:
+            printf("\nFirst value from th e list has been deleted");
+            deleteAtStart(&head);
+            break;
+
+        case 5:
+            printf("\nValue is deleted from the end");
+            deleteAtEnd(&head);
+            break;
+
+        case 6:
+            printf("Enter the element After which you wanted to add the value and new value you want to add : ");
+            scanf("%d %d", &item, &val);
+            insertAfter(&head, item, val);
+            break;
+
+        case 7:
+            printf("Enter the element Before which you wanted to add the value and new value you want to add : ");
+            scanf("%d %d", &item, &val);
+            insertBefore(&head, item, val);
+
+            break;
+
+        case 8:
+            printf("\nLinked list deleted.");
+            deleteList(&head);
+            break;
+
+        case 9:
+            printf("\nEnter the value whoese address you want : ");
+            scanf("%d", &item);
+            node *address = search(head, item);
+            printf("\n%u is the addres of %d", address, item);
+            break;
+
+        case 10:
+
+            break;
+
+        case 11:
+
+            break;
+
+        case 12:
+            break;
+
+        case 13:
+            exit(1);
+            break;
+
+        default:
+            printf("\nEnter a valid option");
+            break;
+        }
+    }
+}
+
+void insertAtStart(node **head1, int val)
+{
+
+    node *temp = (node *)malloc(sizeof(node));
+
+    if (temp == NULL)
+    {
+        printf("\noverflow");
+        exit(1);
+    }
+
+    temp->item = val;
+    if (*head1 == NULL)
+    {
+        temp->next = NULL;
+    }
+
+    else
+    {
+        temp->next = *head1;
+    }
+
+    *head1 = temp;
+
+    printf("\nreacheeeeeeeeed");
+}
+
+void show(node *head1)
+{
+
+    // node *temp = head1;
+    // head1 here is local pointer used to travers linked list which has address of first node not head pointer.
+
+    if (head1 == NULL)
+    {
+        printf("\nList is empty.");
+    }
+
+    while (head1 != NULL)
+    {
+        printf("%d->", head1->item);
+        head1 = head1->next;
+    }
+}
+
+void insertAtEnd(node **head1, int val)
+{
+    node *temp = malloc(sizeof(node));
+
+    temp->item = val;
+    temp->next = NULL;
+
+    if (*head1 == NULL)
+    {
+        *head1 = temp;
+    }
+    else
+    {
+        node *loc = (node *)malloc(sizeof(node));
+        loc = *head1;
+        while (loc->next != NULL)
+        {
+            loc = loc->next;
+        }
+        loc->next = temp;
+    }
+}
+
+node *search(node *head1, int val)
+{ // this is call by value.
+
+    // while (head1->item == val) // first condition is only correct.
+    // {
+    //     head1 = head1->next;
+    // }
+
+    while (head1 != NULL)
+    {
+        if (head1->item == val)
+        {
+            // printf("9349u394uhello--");
+
+            return head1;
+        }
+
+        else
+        {
+            // printf("hello--");
+            head1 = head1->next;
+        }
+    }
+    printf("\nitem not found ");
+}
+
+void insertAfter(node **head1, int item, int val)
+{
+
+    node *temp = (node *)malloc(sizeof(node));
+
+    temp->item = val;
+
+    node *loc = search(*head1, item);
+    if (loc == NULL)
+    {
+        printf("\niteam not found");
+    }
+
+    temp->next = loc->next;
+    loc->next = temp;
+}
+
+void deleteAtStart(node **head1)
+{
+
+    // node *temp=*head1; // no need if we use (*head1)->
+
+    if (*head1 == NULL)
+    {
+        printf("\nList is already empty");
+    }
+
+    else
+    {
+        node *temp = *head1;
+        *head1 = (*head1)->next;
+        free(temp);
+    }
+}
+
+void deleteAtEnd(node **head1)
+{
+
+    if (*head1 == NULL)
+    {
+        printf("\nList is empty");
+        exit(1);
+    }
+
+    else if ((*head1)->next == NULL)
+    {
+        free(*head1);
+        *head1 = NULL;
+    }
+
+    else
+    {
+        node *temp = *head1;
+
+        while ((temp->next)->next != NULL)
+        {
+            temp = temp->next;
+        }
+        free(temp->next);
+        temp->next = NULL;
+    }
+}
+
+void insertBefore(node **head1, int item, int val)
+{
+    if (*head1 == NULL)
+    {
+        printf("\nList is empty");
+        // exit(0);
+    }
+
+    else if ((*head1)->next == NULL)
+    {
+
+        if ((*head1)->item != item)
+        {
+            printf("Iteam not found");
+        }
+
+        else // in this case we could have called insert at start.
+        {
+            node *newnode = (node *)malloc(sizeof(node));
+            newnode->item = val;
+            newnode->next = *head1;
+            *head1 = newnode;
+            // old*head1->next already had a null.
+        }
+    }
+
+    else
+    {
+        node *temp = *head1;
+
+        while ((temp->next)->item != item)
+        {
+            temp = temp->next;
+        }
+
+        node *newnode = (node *)malloc(sizeof(node));
+        newnode->item = val;
+        newnode->next = temp->next;
+        temp->next = newnode;
+    }
+}
+
+void deleteList(node **head1)
+{
+    node *temp, *loc;
+    temp = *head1;
+
+    if (*head1 == NULL)
+    {
+        printf("\nList is allready empty");
+    }
+
+    while (temp->next != NULL)
+    {
+        // order of below statements is rally important.
+        loc = temp;        // give current node address to loc
+        temp = temp->next; // give next node address to temp.
+        free(loc);         // free the current node.
+
+        // if u free the current node first then it is unsafe or impossiable to find next node address.
+
+        printf("\nwhile");
+    }
+    printf("\nwhile out ");
+
+    free(temp);
+    *head1 = NULL;
+}
+
+
+
+
+
+// sumit delete entire code writing comments to understand
+//  void delete_entire(node **head1){
+//      node *temp = *head1;
+//      node *loc;
+//      while(temp != NULL){//stop when current node is null
+//          loc = temp->next;//give loc address of next node.
+//          free(temp);//delete current node.
+//          temp = loc;//give temp address of next node
+//      }
+//      *head1 = NULL;
+// }
+
+// void deleteAtEnd2(node *head)
+// {
+
+//     if (head == NULL)
+//     {
+//         printf("\nlist is already enpty: ");
+//         printf("\nhi i am if");
+
+//     }
+
+//     else if (head->next == NULL)
+//     {
+//         free(head);
+//         head==NULL; // but this wont make og head to point to null.
+//         printf("\nhi i am else if");
+
+//     }
+
+//     else
+//     {
+//         printf("\nhi i am else");
+
+//         while ((head->next)->next != NULL)
+//         {
+
+//             printf("\nhi i am while");
+//             head = head->next;
+//         }
+
+//         printf("\nhi i am out side while");
+//         node *temp=head->next;
+//         free(temp);
+//         head->next == NULL; // ****************this is not working or the above one*******************//
+//         printf("\n%d",head->item);
+//         printf("\n%d",head->next);
+
+//     }
+// }
+
+// void deleteAtEnd2(node *temp){
+
+//     if(temp==NULL){
+//         printf("\nList is allready empty");
+//     }
+
+//     else{
+//         while(temp->next!=NULL){
+//             temp=temp->next;
+//         }
+//         free(temp);
+//     }
+// }
+
+// void deleteAtEnd(node **head1){
+
+//     if(*head1==NULL){
+//         printf("\nList is already empty");
+//     }
+//     else{
+//         // while((*head1)->next!=NULL){
+//         //     *head1=(*head1)->next;
+//         // }
+//         // free(head1);
+//         //this changes the og value of head.
+
+//         node *temp=*head1;
+
+//         while(*temp!=NULL){
+//             // actuall no need to use double pointer here since there will be no change in head value.
+//             //no no no what if only one node is there.
+//             //but i dont want to use double here discuss it
+//         }
+//     }
+// }
